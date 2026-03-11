d3ef1069623670ea8ce5f9284b92eec132da2998
cholo-arx <artcholocequena2007@gmail.com>
2026-03-11 17:07:37 +0800

    using a condition to display if the inputs is not equal

---
+++ displaying_if_number_is_not_equal.py
@@ -0,0 +1,7 @@
+# 2. Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
+# ask for user's input
+number_1 = float(input("input the first number: "))
+number_2 = float(input("input the second number: "))
+
+if number_1 != number_2:
+    print("Not equal")
