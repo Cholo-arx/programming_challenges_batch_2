d10c82e73ff7b9f0b277cdd521804a8f1c88cc83
cholo-arx <artcholocequena2007@gmail.com>
2026-03-11 14:56:04 +0800

    perform a condition

---
+++ displaying_smaller_number_between_two_number.py
@@ -0,0 +1,10 @@
+# 1. Create a program that ask user to input 2 numbers. Print the smaller number.
+# ask for input
+number_1 = float(input("input the first number: "))
+number_2 = float(input("input the second number: "))
+
+# condition
+if number_1 < number_2:
+    print(number_1)
+else:
+    print(number_2)
