229684752a0897a56d8519222e0c1ab3a037a12c
cholo-arx <artcholocequena2007@gmail.com>
2026-03-11 17:52:35 +0800

    same process of quotient_without_decimal; used while loop to eliminate 0 input

---
+++ remainder_of_two_divided_numbers.py
@@ -0,0 +1,13 @@
+# 5. Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+# ask for input
+number_1 = int(input("input the first number: "))
+number_2 = int(input("input the second number: "))
+
+# use while loop to re-enter an input if the user typed 0 in number_2
+while number_2 == 0:
+    print("any number divided by zero is undefined")
+    number_2 = int(input("input the second number: "))
+
+# proceed doing the operation when while successfully meet
+print(
+    f"the remainder of {number_1} and {number_2} when divided is {number_1 % number_2}")
