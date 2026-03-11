6506f79e7417f4f6a1f933800cc3eff8b772f6ab
cholo-arx <artcholocequena2007@gmail.com>
2026-03-11 17:26:19 +0800

    remove difference_of_two_numbers.py

--- difference_of_two_numbers.py
+++
@@ -1,5 +0,0 @@
-# 3.Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
-number_1 = float(input("input the first number: "))
-number_2 = float(input("input the second number: "))
-
-print(f"the difference of {number_1} and {number_2} is {number_1 - number_2}")
